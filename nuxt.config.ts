be8ae95b2a0aa100d3c0f926a2ab13972b01e407
Nebel MASS <52921005+Duorly@users.noreply.github.com>
2023-07-04 11:26:34 +0200

    Config and menu designb

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    css: [
+        'assets/css/main.css'
+    ],
+    devtools: {enabled: true},
+    modules: [
+        '@nuxt/devtools',
+        '@nuxthq/ui',
+        'nuxt-icon',
+        '@nuxtjs/google-fonts',
+        '@dargmuesli/nuxt-cookie-control',
+        '@vueuse/nuxt',
+        // '@nuxtjs/supabase'
+    ],
+    colorMode: {
+        preference: 'system', // default value of $colorMode.preference
+        fallback: 'light', // fallback value if not system preference found
+        hid: 'nuxt-color-mode-script',
+        globalName: '__NUXT_COLOR_MODE__',
+        componentName: 'ColorScheme',
+        classPrefix: '',
+        storageKey: 'theme-color'
+    },
+    googleFonts: {
+        prefetch: true,
+        families: {
+            Montserrat: true,
+            Inter: true,
+            'Hind Madurai': true,
+        }
+    }
 })
