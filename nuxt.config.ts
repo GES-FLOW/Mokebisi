732df8d2c5357f9b5b85c3713ac699edc961f25a
Merite15 <86039889+Merite15@users.noreply.github.com>
2023-07-15 16:30:35 +0100

    upgrade dependencies and add meta seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+     app: {
+        head: {
+            title: "MOKEBISI",
+            meta: [
+                {
+                    charset: "utf-8",
+                },
+                {
+                    name: "viewport",
+                    content: "width=device-width, initial-scale=1",
+                },
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "MOKEBISI",
+                },
+            ],
+            link: [{rel: "icon", type: "image/x-icon", href: "/logo.ico"}],
+        },
+    },
     css: [
         'assets/css/main.css'
     ],
--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,12 @@
         <nav class="w-full flex justify-between items-center">
           <div class="flex items-center md:hidden">
             <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-              <Icon name="mdi:apps" size="30"/>
+              <Icon name="mdi:apps" size="30" />
             </UDropdown>
           </div>
           <div class="flex justify-between items-center space-x-8">
             <div class="flex space-x-2 items-center">
-              <Icon name="mdi:message-alert-outline" size="30"/>
+              <Icon name="mdi:message-alert-outline" size="30" />
               <h1 class="font-[Montserrat] font-bold text-lg">Mokebisi</h1>
 
             </div>
@@ -21,19 +21,19 @@
           </div>
           <div class="flex justify-between items-center">
             <div
-                @click="() => $colorMode.value === 'dark' ? $colorMode.preference = 'light' : $colorMode.preference = 'dark'"
-                class="flex items-center justify-center h-10 w-10 hover:bg-blue-200/50 hover:dark:bg-gray-100/10 rounded-full cursor-pointer">
+              @click="() => $colorMode.value === 'dark' ? $colorMode.preference = 'light' : $colorMode.preference = 'dark'"
+              class="flex items-center justify-center h-10 w-10 hover:bg-blue-200/50 hover:dark:bg-gray-100/10 rounded-full cursor-pointer">
               <Icon v-show="$colorMode.value === 'light'" name="line-md:moon-filled-alt-to-sunny-filled-loop-transition"
-                    size="25"/>
-              <Icon v-show="$colorMode.value === 'dark'" name="line-md:moon-loop" size="25"/>
+                size="25" />
+              <Icon v-show="$colorMode.value === 'dark'" name="line-md:moon-loop" size="25" />
             </div>
             <div
-                class="flex items-center justify-center h-10 w-10 hover:bg-blue-200/50 hover:dark:bg-gray-100/10 rounded-full cursor-pointer">
-              <Icon name="mdi:twitter" size="25"/>
+              class="flex items-center justify-center h-10 w-10 hover:bg-blue-200/50 hover:dark:bg-gray-100/10 rounded-full cursor-pointer">
+              <Icon name="mdi:twitter" size="25" />
             </div>
             <div
-                class="hidden md:flex items-center justify-center h-10 w-10 hover:bg-blue-200/50 hover:dark:bg-gray-100/10 rounded-full cursor-pointer">
-              <Icon name="mdi:github" size="25"/>
+              class="hidden md:flex items-center justify-center h-10 w-10 hover:bg-blue-200/50 hover:dark:bg-gray-100/10 rounded-full cursor-pointer">
+              <Icon name="mdi:github" size="25" />
             </div>
           </div>
         </nav>
@@ -47,15 +47,16 @@
 
           <p class="text-center text-lg text-muted-foreground sm:text-xl">
             Tout en restant anonyme, venez nous dire ce qui vous énerve au 242
-          </p></div>
+          </p>
+        </div>
         <div class="flex flex-col justify-center gap-2 sm:flex-row sm:gap-4">
           <NuxtLink
-              class="inline-flex items-center justify-center rounded-2xl text-sm text-white font-medium bg-blue-500 text-primary-foreground hover:bg-blue-500/90 h-11 px-8">
+            class="inline-flex items-center justify-center rounded-2xl text-sm text-white font-medium bg-blue-500 text-primary-foreground hover:bg-blue-500/90 h-11 px-8">
             Voir les messages
           </NuxtLink>
           <NuxtLink
-              class="inline-flex items-center justify-center rounded-2xl text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background border border-input hover:bg-accent hover:text-accent-foreground h-11 px-8"
-              href="https://github.com/ln-dev7/camerenerve">Contribuer sur
+            class="inline-flex items-center justify-center rounded-2xl text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background border border-input hover:bg-accent hover:text-accent-foreground h-11 px-8"
+            href="https://github.com/ln-dev7/camerenerve">Contribuer sur
             GitHub
           </NuxtLink>
         </div>
@@ -67,29 +68,29 @@
           <div class="flex flex-wrap w-full items-center justify-between py-4">
             <div class="w-full md:w-[45%] lg:w-[30%] my-2">
               <UCard>
-                <template #header/>
+                <template #header />
 
                 Body
 
-                <template #footer/>
+                <template #footer />
               </UCard>
             </div>
             <div class="w-full md:w-[45%] lg:w-[30%] my-2">
               <UCard>
-                <template #header/>
+                <template #header />
 
                 Body
 
-                <template #footer/>
+                <template #footer />
               </UCard>
             </div>
             <div class="w-full md:w-[45%] lg:w-[30%] my-2">
               <UCard>
-                <template #header/>
+                <template #header />
 
                 Body
 
-                <template #footer/>
+                <template #footer />
               </UCard>
             </div>
 
@@ -131,4 +132,21 @@ const items = [
     shortcuts: ['⌘', 'D']
   }]
 ]
+
+useSeoMeta({
+  ogTitle: 'Accueil',
+  ogDescription: 'Accueil',
+  ogImage: '[og:image]',
+  ogUrl: '[og:url]',
+  twitterTitle: '[twitter:title]',
+  twitterDescription: '[twitter:description]',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary',
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'fr',
+  },
+})
 </script>
